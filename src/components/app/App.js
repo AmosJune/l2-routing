af0a04ae962afb246ff09c07f0ed9ee026719a29
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-20 13:16:52 +0300

    Done

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,9 @@ import Navbar from '../navbar/Navbar';
 import Contact from '../contact/Contact';
 import About from '../about/About';
 import Home from '../home/Home';
+import User from '../user/User';
+import { MemoryRouter as Router, Link, Routes, Route } from 'react-router';
+//for every link you need a route
 
 function App() {
   return (
@@ -15,6 +18,28 @@ function App() {
   );
 }
 
+function MainApp() {
+  <Router>
+    <div>
+      {/* All Routes go here */}
+
+      <div className="topNavBar">
+        <Link className="active" to="/">Home</Link>.
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+        <Link to="/user/faith">USER FAITH</Link>
+      </div>
+        
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/contact' element={<Contact/>}/>
+        <Route exact path='/about' element={<About/>}/>
+        <Route path='/user/:username' element={<User/>}/>
+      </Routes>
+    </div>
+  </Router>
+}
 
 
 export default App;
+export { MainApp }; 
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { useParams } from "react-router";
 
 export default function User(){
+
+    let { username } = useParams()
 
     return(
         <div>
             <center style={{marginTop: 5 +'em', fontSize: 30 + 'px'}}>
-                User: 
+                User: {username}
             </center>
         </div>
     )
 
 }
+
+
+// //.Extra
+// function Doubler() {
+
+//     let { num } = useParams()
+
+
+//     return ( 
+//         <div>
+//             Double of {} is {}
+//         </div>
+//     )
+// }
